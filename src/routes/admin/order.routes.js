d2bb2cb79ac306cd0fc519aafa6eb6533377d147
Chandan Mahato <cmahato2000@gmail.com>
2021-07-04 21:15:20 +0545

    order update route

--- src/routes/admin/order.routes.js
+++ src/routes/admin/order.routes.js
@@ -1,8 +1,9 @@
-const { requireSignin, adminMiddleware } = require("../../common-middleware");
-const { getCustomerOrders } = require("../../controller/admin/order.admin");
+const { requireAdminSignin, adminMiddleware } = require("../../common-middleware");
+const { getCustomerOrders, adminUpdateOrder } = require("../../controller/admin/order.admin");
 
 const router = require("express").Router();
 
-router.post(`/admin/getOrder`, requireSignin, adminMiddleware, getCustomerOrders);
+router.post(`/admin/getOrder`, requireAdminSignin, adminMiddleware, getCustomerOrders);
+router.post(`/admin/order/update`, requireAdminSignin, adminMiddleware, adminUpdateOrder);
 
 module.exports = router;
