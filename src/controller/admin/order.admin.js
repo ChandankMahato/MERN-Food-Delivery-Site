348bb98bc3143b9439415dfeadf8bb50a522a0c1
Chandan Mahato <cmahato2000@gmail.com>
2021-07-04 21:16:52 +0545

    admin order update

--- src/controller/admin/order.admin.js
+++ src/controller/admin/order.admin.js
@@ -10,3 +10,21 @@ exports.getCustomerOrders = async (req, res) => {
     .exec();
     res.status(200).json({ orders });
 }
+
+exports.adminUpdateOrder = (req, res) => {
+    Order.updateOne(
+        {_id: req.body.orderId, "orderStatus.type": req.body.type},
+        {
+            $set:{
+                "orderStatus.$": [{type: req.body.type, date: new Date(),isCompleted: true}],
+            },
+        }
+    ).exec((error, order) => {
+        if(error) return res.status(400).json({error});
+        if(order){
+            res.status(201).json({order});
+        }
+    });
+}
+
+
