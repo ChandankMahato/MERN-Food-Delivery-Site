b61a2811e84658876fafe9d386c2ca4c163b5e66
Chandan Mahato <cmahato2000@gmail.com>
2021-07-04 21:19:51 +0545

    axios admin

---
+++ front_end/src/helpers/adminAxios.js
@@ -0,0 +1,38 @@
+
+import axios from 'axios';
+import { api } from '../urlConfig';
+import store from '../store';
+import { authConstants } from '../actions/constants';
+
+const adminToken = window.localStorage.getItem('adminToken');
+
+const adminAxiosIntance = axios.create({
+  baseURL: api,
+  headers: {
+    'Adminauthorization': adminToken ? `Bearer ${adminToken}` : ''
+  }
+});
+
+adminAxiosIntance.interceptors.request.use((req) => {
+  const {adminAuth} = store.getState();
+  console.log(adminAuth.adminToken);
+  if(adminAuth.adminToken){
+    req.headers.Adminauthorization = `Bearer ${adminAuth.adminToken}`;
+  }
+  return req;
+})
+
+
+adminAxiosIntance.interceptors.response.use((res) => {
+  return res;
+}, (error) => {
+  console.log(error);
+  const { status } = error.response;
+  if(status === 500) {
+    localStorage.clear();
+    store.dispatch({ type: authConstants.LOGOUT_SUCCESS});
+  }
+  return Promise.reject(error);
+})
+
+export default adminAxiosIntance;
