d7735a99259253b4a9b8adeb9f691a5c69213004
Chandan <63946020+ChandankMahato@users.noreply.github.com>
2021-09-08 09:53:34 +0545

    Update urlConfig.js

--- front_end/src/urlConfig.js
+++ front_end/src/urlConfig.js
@@ -1,5 +1,12 @@
 //this is our api url(baseURL)
-export const api = 'http://localhost:2000/api';
+// export const api = 'http://localhost:2000/api';
+// export const generatePublicUrl = (fileName) => {
+//     return `http://localhost:2000/public/${fileName}`   
+// }
+const baseUrl = "https://bs-gyf.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
+
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`   
+    return `${baseUrl}/public/${fileName}`;
 }
