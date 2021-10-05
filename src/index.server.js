7672a020cdebf7e3fca74805fd20661bb7eaa174
Chandan <63946020+ChandankMahato@users.noreply.github.com>
2021-10-05 07:43:40 +0545

    Update index.server.js

--- src/index.server.js
+++ src/index.server.js
@@ -43,7 +43,6 @@ mongoose.connect(
 
 //middleware
 app.use(cors());
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/public',express.static(path.join(__dirname, 'uploads')));
 app.use('/api', adminAuthRoutes);
